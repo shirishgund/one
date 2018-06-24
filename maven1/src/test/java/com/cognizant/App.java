98fcf1942fafdb227f75c17413b0afd39cd33258
shirishgund <shirishgund@gmail.com>
2018-06-24 22:18:52 +0530

    initial commit

---
+++ maven1/src/test/java/com/cognizant/App.java
@@ -0,0 +1,14 @@
+package com.cognizant;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class App {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
